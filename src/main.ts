62e3520c472e19da2fb6d7bac9382827155c2c1b
suwandre <suwandresukijat@gmail.com>
2026-01-01 20:44:22 +0100

    feat: global DTO validation pipe; added controllers to auth

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strip properties not in DTO
+      transform: true, // Auto-transform to DTO classes
+    }),
+  );
+  await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
