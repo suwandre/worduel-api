2ee45bbc85a2d3841891d8f75ff0295e02510a6c
suwandre <suwandresukijat@gmail.com>
2026-01-01 20:07:09 +0100

    feat: user schema

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,26 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+/** Represents a Worduel user. */
+@Schema({ timestamps: true, collection: 'users' })
+export class User extends Document {
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  username: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ default: 0 })
+  gamesPlayed: number;
+
+  @Prop({ default: 0 })
+  gamesWon: number;
+
+  @Prop({ default: 0 })
+  currentStreak: number;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
