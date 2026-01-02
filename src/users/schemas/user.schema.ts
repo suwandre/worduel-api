add603f915afad721f1363dfbdae296b9104d5c8
suwandre <suwandresukijat@gmail.com>
2026-01-02 12:56:27 +0100

    feat: added guess distribution in user schema

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-@Schema({ timestamps: true, collection: 'users' })
+@Schema({ timestamps: true })
 export class User extends Document {
   @Prop({ required: true, unique: true })
   email: string;
@@ -20,6 +20,13 @@ export class User extends Document {
 
   @Prop({ default: 0 })
   currentStreak: number;
+
+  @Prop({ default: 0 })
+  maxStreak: number;
+
+  /** The amount of times the player guessed a word within 1, 2, ..., or 6 tries. */
+  @Prop({ type: Object, default: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 } })
+  guessDistribution: Record<number, number>;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
