1da3cf15c5077ffd0632b4f412c2de39d5a1c202
suwandre <suwandresukijat@gmail.com>
2026-01-02 22:38:13 +0100

    feat: added cors to localhost

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,17 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  if (!process.env.JWT_SECRET) {
+    throw new Error('JWT_SECRET is not defined in environment variables');
+  }
+
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'], // Vite default port - will change later
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Strip properties not in DTO
